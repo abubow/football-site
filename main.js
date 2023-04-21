8647332b52a3d0cd203161f7d168e44c8c33a819
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-04-21 22:45:57 +0500

    added Information.html and finished index.html

--- main.js
+++ main.js
@@ -7,7 +7,27 @@ toggleButton.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
 
-// Make the navbar sticky when scrolling
+// // Make the navbar sticky when scrolling
+// window.addEventListener('scroll', () => {
+//   navbar.classList.toggle('sticky', window.scrollY > 0);
+// });
+// previous scroll position
+let prevScrollpos = window.pageYOffset;
+// Hide the navbar when scrolling down using hide class and show it when scrolling up using sticky class
 window.addEventListener('scroll', () => {
-  navbar.classList.toggle('sticky', window.scrollY > 0);
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        navbar.classList.add('sticky');
+        navbar.classList.remove('hide');
+    } else {
+        navbar.classList.remove('sticky');
+        navbar.classList.add('hide');
+    }
+    prevScrollpos = currentScrollPos;
+});
+// Close the menu when clicking outside
+window.addEventListener('click', (e) => {
+    if (e.target.closest('.navbar-toggle') === null && e.target.closest('.navbar-menu') === null) {
+        menu.classList.remove('show');
+    }
 });
