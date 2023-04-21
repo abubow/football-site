c5996b728e67dd7b053abf814559fe1f8bc0b975
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-04-21 23:28:20 +0500

    added about and reviews section

---
+++ data.js
@@ -0,0 +1,62 @@
+window.reviewData = [
+	{
+		name: "Sana",
+		date: "2022-02-01",
+		rating: 4,
+		review: "I love playing football, and this site is an excellent platform to share my experiences with fellow enthusiasts.",
+	},
+	{
+		name: "Ali",
+		date: "2022-02-04",
+		rating: 5,
+		review: "I've been playing football for years, and this site has given me a chance to connect with people who share the same passion. Highly recommended!",
+	},
+	{
+		name: "Nida",
+		date: "2022-02-07",
+		rating: 3,
+		review: "The site is good, but it can be hard to navigate at times. Overall, I've had a decent experience playing football here.",
+	},
+	{
+		name: "Ahmed",
+		date: "2022-02-10",
+		rating: 5,
+		review: "This site is the best! I've made so many new friends and improved my skills so much thanks to the community here.",
+	},
+	{
+		name: "Zainab",
+		date: "2022-02-13",
+		rating: 4,
+		review: "I'm a newbie to football, and this site has been so helpful in guiding me. I've learned so much from the tutorials and tips provided.",
+	},
+	{
+		name: "Hassan",
+		date: "2022-02-16",
+		rating: 2,
+		review: "I've had some negative experiences playing football here, mainly due to the behavior of some players. The site needs to do more to monitor and prevent such incidents.",
+	},
+	{
+		name: "Ayesha",
+		date: "2022-02-19",
+		rating: 5,
+		review: "Football is my passion, and this site is a dream come true for me. I can't thank the admins enough for creating such a wonderful platform.",
+	},
+	{
+		name: "Bilal",
+		date: "2022-02-22",
+		rating: 3,
+		review: "The site is good, but I wish there were more options for customization and personalization. It would be great to be able to create and join leagues with specific rules and settings.",
+	},
+	{
+		name: "Fatima",
+		date: "2022-02-25",
+		rating: 4,
+		review: "I've been able to improve my skills so much thanks to the feedback and advice provided by other players on this site. It's like having a personal coach!",
+	},
+	{
+		name: "Imran",
+		date: "2022-02-28",
+		rating: 4,
+		review: "I've had a great time playing football on this site, and the competitive atmosphere has really pushed me to improve my game. Can't wait to see what the future holds!",
+	},
+];
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const navbar = document.querySelector('.navbar');
-const toggleButton = navbar.querySelector('.navbar-toggle');
-const menu = navbar.querySelector('.navbar-menu');
+const navbar = document.querySelector(".navbar");
+const toggleButton = navbar.querySelector(".navbar-toggle");
+const menu = navbar.querySelector(".navbar-menu");
 
 // Toggle the menu on button click
-toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('show');
+toggleButton.addEventListener("click", () => {
+	menu.classList.toggle("show");
 });
 
 // // Make the navbar sticky when scrolling
@@ -14,20 +14,104 @@ toggleButton.addEventListener('click', () => {
 // previous scroll position
 let prevScrollpos = window.pageYOffset;
 // Hide the navbar when scrolling down using hide class and show it when scrolling up using sticky class
-window.addEventListener('scroll', () => {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        navbar.classList.add('sticky');
-        navbar.classList.remove('hide');
-    } else {
-        navbar.classList.remove('sticky');
-        navbar.classList.add('hide');
-    }
-    prevScrollpos = currentScrollPos;
+window.addEventListener("scroll", () => {
+	let currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		navbar.classList.add("sticky");
+		navbar.classList.remove("hide");
+	} else {
+		navbar.classList.remove("sticky");
+		navbar.classList.add("hide");
+	}
+	prevScrollpos = currentScrollPos;
 });
 // Close the menu when clicking outside
-window.addEventListener('click', (e) => {
-    if (e.target.closest('.navbar-toggle') === null && e.target.closest('.navbar-menu') === null) {
-        menu.classList.remove('show');
-    }
+window.addEventListener("click", (e) => {
+	if (
+		e.target.closest(".navbar-toggle") === null &&
+		e.target.closest(".navbar-menu") === null
+	) {
+		menu.classList.remove("show");
+	}
+});
+
+filledStar = "★";
+emptyStar = "☆";
+
+// Function to calculate the average rating
+function calculateAverageRating() {
+	let total = 0;
+	let count = 0;
+	for (let i = 0; i < reviewData.length; i++) {
+		total += reviewData[i].rating;
+		count++;
+	}
+	return total / count;
+}
+
+const averageRatingContainer = document.getElementById("average-rating");
+// Function to display the average rating
+function displayAverageRating() {
+	const averageRating = calculateAverageRating();
+    console.log(averageRating)
+    console.log(averageRatingContainer)
+	averageRatingContainer.innerHTML =
+		averageRating.toFixed(2) +
+		" " +
+		filledStar.repeat(Math.floor(averageRating)) +
+		emptyStar.repeat(5 - Math.floor(averageRating));
+}
+
+// displayAverageRating();
+// element to show the ratings
+const ratingsContainer = document.getElementById("app");
+
+// Function to display the ratings
+function displayRatings() {
+	let ratingsHTML = "";
+	for (let i = 0; i < reviewData.length; i++) {
+		ratingsHTML += `
+      <div class="rating">
+        <div class="rating-header">
+          <div class="rating-name">${reviewData[i].name}</div>
+          <div class="rating-date">${reviewData[i].date}</div>
+        </div>
+        <div class="rating-stars">
+          ${filledStar.repeat(reviewData[i].rating)}
+          ${emptyStar.repeat(5 - reviewData[i].rating)}
+        </div>
+        <div class="rating-review">${reviewData[i].review}</div>
+      </div>
+    `;
+	}
+    console.log(ratingsHTML);
+	ratingsContainer.innerHTML = ratingsHTML;
+}
+
+displayRatings();
+
+// Input fields
+const form = document.getElementById("review-form");
+const nameInput = document.getElementById("name");
+const reviewInput = document.getElementById("review");
+const ratingInput = document.getElementById("rating");
+const submitButton = document.getElementById("submit");
+
+// Function to add a new rating
+function addRating() {
+	const newRating = {
+		name: nameInput.value,
+		review: reviewInput.value,
+		rating: parseInt(ratingInput.value),
+		date: new Date().toLocaleDateString(),
+	};
+	reviewData.push(newRating);
+	displayRatings();
+	displayAverageRating();
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	addRating();
+	form.reset();
 });
